6b03bc74ab8f9b2b9674e9d2d9932734a020a22e
Chris Yang <cyang287@yahoo.com>
2018-04-13 14:22:10 -0400

    finished actions

---
+++ src/actions/cat.js
@@ -0,0 +1,42 @@
+import {API_BASE_URL} from '../config';
+
+export const FETCH_CAT_REQUEST = 'FETCH_CAT_REQUEST'; 
+export const fetchCatRequest = () => ({
+  type: FETCH_CAT_REQUEST,
+});
+
+export const FETCH_CAT_SUCCESS = 'FETCH_CAT_SUCCESS'; 
+export const fetchCatSuccess = () => ({
+  type: FETCH_CAT_SUCCESS,
+});
+
+export const FETCH_CAT_ERROR = 'FETCH_CAT_ERROR'; 
+export const fetchCatError = () => ({
+  type: FETCH_CAT_ERROR,
+});
+
+export const fetchCat = cat => (dispatch) => {
+  dispatch(fetchCatRequest());
+  return fetch(`${API_BASE_URL}/cat`, {
+    method: 'GET',
+  })
+    .then(res => {
+      return res.json();
+    })
+    .then(cat => {
+      dispatch(fetchCatSuccess(cat));
+    })
+    .catch(err => {dispatch(fetchCatError(err))});
+}
+
+export const adoptCat = cat => (dispatch) => {
+  return fetch(`${API_BASE_URL}/cat`, {
+    method: 'DELETE',
+  })
+    .then(res => {
+      return res.json();
+    })
+    .then(() => {
+      fetchCat();
+    })
+}
---
+++ src/actions/dog.js
@@ -0,0 +1,42 @@
+import {API_BASE_URL} from '../config';
+
+export const FETCH_DOG_REQUEST = 'FETCH_DOG_REQUEST'; 
+export const fetchDogRequest = () => ({
+  type: FETCH_DOG_REQUEST,
+});
+
+export const FETCH_DOG_SUCCESS = 'FETCH_DOG_SUCCESS'; 
+export const fetchDogSuccess = () => ({
+  type: FETCH_DOG_SUCCESS,
+});
+
+export const FETCH_DOG_ERROR = 'FETCH_DOG_ERROR'; 
+export const fetchDogError = () => ({
+  type: FETCH_DOG_ERROR,
+});
+
+export const fetchDog = dog => (dispatch) => {
+  dispatch(fetchDogRequest());
+  return fetch(`${API_BASE_URL}/dog`, {
+    method: 'GET',
+  })
+    .then(res => {
+      return res.json();
+    })
+    .then(dog => {
+      dispatch(fetchDogSuccess(dog));
+    })
+    .catch(err => {dispatch(fetchDogError(err))});
+}
+
+export const adoptDog = dog => (dispatch) => {
+  return fetch(`${API_BASE_URL}/dog`, {
+    method: 'DELETE',
+  })
+    .then(res => {
+      return res.json();
+    })
+    .then(() => {
+      fetchDog();
+    })
+}
