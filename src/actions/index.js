34b6ce0d7305ca1576bde02edc7b635fb39045ad
alishaevn <alieva.inc@gmail.com>
2018-04-13 12:14:30 -0700

    added

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,4 +2,6 @@ import * as catActions from './cat'
 
 import * as dogActions from './dog'
 
-export default { ...dogActions, ...catActions }
+// export default { ...dogActions, ...catActions }
+
+export default { ...dogActions, catActions }
