a4cdd4f9f2840cd7da7c3f152c94955391781deb
Chris Yang <cyang287@yahoo.com>
2018-04-13 13:55:49 -0400

    changed some things

---
+++ src/components/Dashboard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        Hello
+      </div>
+    ) 
+  }
+}
+
+
+export default Dashboard;
